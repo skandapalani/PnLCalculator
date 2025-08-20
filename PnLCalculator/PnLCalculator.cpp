a977590c835d333fef4d4a10eb1f27c98d40b5e3
Skanda <Skanda@DESKTOP-5GCG3ED>
2025-08-20 10:14:30 -0400

    Add project files.

---
+++ PnLCalculator/PnLCalculator.cpp
@@ -0,0 +1,12 @@
+﻿// PnLCalculator.cpp : Defines the entry point for the application.
+//
+
+#include "PnLCalculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
